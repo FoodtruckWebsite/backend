70976efb19b1b30af27f4c6738007cea7ef6ed3b
ogonz801 <g.osvaldo96@gmail.com>
2022-07-26 17:59:47 -0600

    updated User models and fixed bug of data not being added to mongoDB

--- models/User.js
+++ models/User.js
@@ -5,9 +5,7 @@ const UserSchema = new mongoose.Schema({
     username:  {type: String, required: true},
     email:  {type: String, required: true},
     password:  {type: String, required: true},
-    owner: [{
-        type: Boolean
-    }]
+    owner: {type: Boolean, default: false, required: true}
 })
 
 const User = mongoose.model('User', UserSchema)
