25103e10a170197e55aba9a427981320ad46f71e
ogonz801 <g.osvaldo96@gmail.com>
2022-07-26 17:20:45 -0600

    connected to mongoDB atlas

--- config/connections.js
+++ config/connections.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/NOW', {
+mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
 
-mongoose.connection.on('connected', () => console.log('connected to mongoose'))
+mongoose.connection.on('connected', () => console.log('connected to mongodb'))
