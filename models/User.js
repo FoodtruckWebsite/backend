88a2ac4b2debccbf05ca1e424126b07729feb967
ogonz801 <g.osvaldo96@gmail.com>
2022-07-26 19:06:29 -0600

    installed and implemented bcrypt to User model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
     avatar: String,
@@ -7,6 +8,33 @@ const UserSchema = new mongoose.Schema({
     password:  {type: String, required: true},
     owner: {type: Boolean, default: false, required: true}
 })
+
+UserSchema.pre('save', function(next) {
+    const user = this
+    if(!user.isModified('password'))
+        return next()
+    bcrypt.hash(user.password, 10, (err, passwordHash) => {
+        if (err) {
+            return next(err)
+        }
+        user.password = passwordHash
+        next()
+    })
+})
+
+UserSchema.methods.comparePassword = function(password, cb) {
+    const user = this
+    bcrypt.compare(password, user.password, (err, isMatch) => {
+        if (err) {
+            return cb(err)
+        }
+        else {
+            if(!isMatch) {
+                return cb(null, isMatch)
+        }
+        return cb(null, user)
+    }})
+}
 
 const User = mongoose.model('User', UserSchema)
 
