8e845b95909bcadd53ff201daa4f11e471a206a7
ogonz801 <g.osvaldo96@gmail.com>
2022-07-27 18:19:31 -0600

    createUser function updated

--- controllers/userCtrls.js
+++ controllers/userCtrls.js
@@ -4,12 +4,25 @@ const passportConfig = require('../config/passport')
 const JWT = require('jsonwebtoken');
 
 const createUser = (req, res) => {
-    User.create(req.body, (err, createdUser) => {
+    User.findOne({username: req.body.username}, (err, user) => {
         if (err) {
-            res.status(400).json(err)
+            res.status(500).json({message: 'error has occured'})
             return
         }
-        res.json(createdUser)
+        if (user) {
+            res.status(400).json({message: 'User already exists'})
+            return
+        }
+        else {
+            User.create(req.body, (err, createdUser) => {
+                if (err) {
+                    res.status(400).json({message: 'error creating user'})
+                }
+                else {
+                    res.status(200).json({message: 'account created'})
+                }
+            })
+        }
     })
 }
 const deleteUser = (req, res) => {
